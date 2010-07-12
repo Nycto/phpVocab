14133ab711c1560803afd048d8728aa9836e357a
Nycto <James@RoundEights.com>
2010-07-11 17:44:29 -0700

    Added Data\Type\Cls

---
+++ src/classes/Data/Type/Cls.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of phpVocab.
+ *
+ * phpVocab is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * phpVocab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with phpVocab. If not, see <http://www.phpVocab.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ */
+
+namespace vc\Data\Type;
+
+/**
+ * A class
+ */
+class Cls extends \vc\Data\Type
+{
+
+    /**
+     * Whether this method is flagged as final
+     *
+     * @var Boolean
+     */
+    private $final = FALSE;
+
+    /**
+     * Whether this method is abstract
+     *
+     * @var Boolean
+     */
+    private $abstract = FALSE;
+
+    /**
+     * The parent of this class
+     *
+     * @var String
+     */
+    private $extends;
+
+    /**
+     * The interface this string implements
+     *
+     * @var Array
+     */
+    private $implements = array();
+
+    /**
+     * Sets whether this method is final
+     *
+     * @param Boolean $final
+     * @return \vc\Data\Method Returns a self reference
+     */
+    public function setFinal ( $final )
+    {
+        $this->final = (bool) $final;
+        return $this;
+    }
+
+    /**
+     * Returns whether this method is final
+     *
+     * @return Boolean
+     */
+    public function getFinal ()
+    {
+        return $this->final;
+    }
+
+    /**
+     * Sets whether this method is abstract
+     *
+     * @param Boolean $abstract
+     * @return \vc\Data\Method Returns a self reference
+     */
+    public function setAbstract ( $abstract )
+    {
+        $this->abstract = (bool) $abstract;
+        return $this;
+    }
+
+    /**
+     * Returns whether this method is abstract
+     *
+     * @return Boolean
+     */
+    public function getAbstract ()
+    {
+        return $this->abstract;
+    }
+
+    /**
+     * Sets the parent of this class
+     *
+     * @param String $extends
+     * @return \vc\Data\Type\Cls Returns a self reference
+     */
+    public function setExtends ( $extends )
+    {
+        $this->extends = $extends;
+        return $this;
+    }
+
+    /**
+     * Returns the parent of this class
+     *
+     * @return String
+     */
+    public function getExtends ()
+    {
+        return $this->extends;
+    }
+
+    /**
+     * Adds a new interface that this class implements
+     *
+     * @param String $implements
+     * @return \vc\Data\Type\Cls Returns a self reference
+     */
+    public function addIFace ( $iface )
+    {
+        $this->implements[] = $iface;
+        return $this;
+    }
+
+    /**
+     * Returns the interfaces this class implements
+     *
+     * @return Array
+     */
+    public function getIFaces ()
+    {
+        return $this->implements;
+    }
+
+}
+
+?>
---
+++ tests/classes/Data/Type/Cls.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of phpVocab.
+ *
+ * phpVocab is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * phpVocab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with phpVocab. If not, see <http://www.phpVocab.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../../setup.php";
+
+/**
+ * Unit test
+ */
+class test_classes_Data_Type_Cls extends PHPUnit_Framework_TestCase
+{
+
+    public function testFinalAccess ()
+    {
+        $cls = $this->getMockForAbstractClass(
+            '\vc\Data\Type\Cls',
+            array(123, new \vc\Data\Comment("summary", "details"))
+        );
+
+        $this->assertFalse( $cls->getFinal() );
+        $this->assertSame( $cls, $cls->setFinal(TRUE) );
+        $this->assertTrue( $cls->getFinal() );
+    }
+
+    public function testAbstractAccess ()
+    {
+        $cls = $this->getMockForAbstractClass(
+            '\vc\Data\Type\Cls',
+            array(123, new \vc\Data\Comment("summary", "details"))
+        );
+
+        $this->assertFalse( $cls->getAbstract() );
+        $this->assertSame( $cls, $cls->setAbstract(TRUE) );
+        $this->assertTrue( $cls->getAbstract() );
+    }
+
+    public function testExtendsAccess ()
+    {
+        $cls = $this->getMockForAbstractClass(
+            '\vc\Data\Type\Cls',
+            array(123, new \vc\Data\Comment("summary", "details"))
+        );
+
+        $this->assertNull( $cls->getExtends() );
+        $this->assertSame( $cls, $cls->setExtends('parent') );
+        $this->assertSame( 'parent', $cls->getExtends() );
+    }
+
+    public function testIFaceAccess ()
+    {
+        $cls = $this->getMockForAbstractClass(
+            '\vc\Data\Type\Cls',
+            array(123, new \vc\Data\Comment("summary", "details"))
+        );
+
+        $this->assertSame( array(), $cls->getIFaces() );
+        $this->assertSame( $cls, $cls->addIFace('parent') );
+        $this->assertSame( array('parent'), $cls->getIFaces() );
+        $this->assertSame( $cls, $cls->addIFace('parent2') );
+        $this->assertSame( array('parent', 'parent2'), $cls->getIFaces() );
+    }
+
+}
+
+?>
