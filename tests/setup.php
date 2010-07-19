b11623be6a0405c96001141497bc4b52ccc806bd
Nycto <James@RoundEights.com>
2010-07-18 18:19:08 -0700

    Updated setup script to exclude base TestCase from backtraces

--- tests/setup.php
+++ tests/setup.php
@@ -38,5 +38,6 @@ require_once rtrim( __DIR__, "/" ) ."/../src/vocab.php";
 error_reporting( E_ALL | E_STRICT );
 
 PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'PHPUNIT');
+PHPUnit_Util_Filter::addDirectoryToFilter(rtrim( __DIR__, "/" ) .'/Test');
 
 ?>
