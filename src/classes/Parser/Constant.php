874850548d15403a19ae5c9e82c7fb2c161168eb
Nycto <James@RoundEights.com>
2010-07-28 08:22:27 -0700

    Added Parser\Constant

---
+++ src/classes/Parser/Constant.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of phpVocab.
+ *
+ * phpVocab is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * phpVocab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with phpVocab. If not, see <http://www.phpVocab.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ */
+
+namespace vc\Parser;
+
+use \vc\Tokens\Token as Token;
+
+/**
+ * Parses an Alias declaration
+ */
+class Constant
+{
+
+    /**
+     * The parser for constructing value objects
+     *
+     * @var \vc\Parser\Value
+     */
+    private $value;
+
+    /**
+     * Constructor...
+     *
+     * @param \vc\Parser\Value $value The parser for constructing value objects
+     */
+    public function __construct ( \vc\Parser\Value $value )
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Parses the given token reader
+     *
+     * @param \vc\Tokens\Access $access The token access
+     * @return \vc\Data\Constant Returns the created constant
+     */
+    public function parseConstant ( \vc\Tokens\Access $access )
+    {
+        $name = $access->findAllowing(
+            array(Token::T_STRING), array(Token::T_WHITESPACE)
+        );
+
+        $const = new \vc\Data\Constant( $name->getContent() );
+
+        $access->findAllowing(
+            array(Token::T_EQUALS), array(Token::T_WHITESPACE)
+        );
+
+        $const->setValue( $this->value->parseValue($access) );
+
+        $access->findAllowing(
+            array(Token::T_SEMICOLON),
+            array(Token::T_WHITESPACE)
+        );
+
+        return $const;
+    }
+
+}
+
+?>
---
+++ tests/classes/Parser/Constant.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of phpVocab.
+ *
+ * phpVocab is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * phpVocab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with phpVocab. If not, see <http://www.phpVocab.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../setup.php";
+
+use \vc\Tokens\Token as Token;
+
+/**
+ * Unit tests
+ */
+class test_classes_Parser_Constant extends \vc\Test\TestCase
+{
+
+    public function testParseConstant ()
+    {
+        $access = \vc\Tokens\Access::buildAccess(
+            $this->oneTokenReader()
+                ->thenAName('CONSTANT')->thenSomeSpace()
+                ->thenAnEquals()->thenSomeSpace()
+                ->thenAnInteger(123)->thenASemicolon()
+                ->thenAClass()
+        );
+
+        $parser = new \vc\Parser\Constant( new \vc\Parser\Value );
+
+        $this->assertEquals(
+            r8(new \vc\Data\Constant('CONSTANT'))
+                ->setValue(new \vc\Data\Value('123', 'int')),
+            $parser->parseConstant( $access )
+        );
+
+        $this->assertHasToken( Token::T_CLASS, $access );
+    }
+
+    public function testParseConstant_FloatValue ()
+    {
+        $access = \vc\Tokens\Access::buildAccess(
+            $this->oneTokenReader()
+                ->thenAName('NAME')->thenSomeSpace()
+                ->thenAnEquals()->thenSomeSpace()
+                ->thenAFloat(12.3)->thenASemicolon()
+                ->thenAClass()
+        );
+
+        $parser = new \vc\Parser\Constant( new \vc\Parser\Value );
+
+        $this->assertEquals(
+            r8(new \vc\Data\Constant('NAME'))
+                ->setValue(new \vc\Data\Value('12.3', 'float')),
+            $parser->parseConstant( $access )
+        );
+
+        $this->assertHasToken( Token::T_CLASS, $access );
+    }
+
+    public function testParseConstant_StringWithSingleQuotes ()
+    {
+        $access = \vc\Tokens\Access::buildAccess(
+            $this->oneTokenReader()
+                ->thenAName('NAME')->thenSomeSpace()
+                ->thenAnEquals()->thenSomeSpace()
+                ->thenAString('string', "'")->thenASemicolon()
+                ->thenAClass()
+        );
+
+        $parser = new \vc\Parser\Constant( new \vc\Parser\Value );
+
+        $this->assertEquals(
+            r8(new \vc\Data\Constant('NAME'))
+                ->setValue(new \vc\Data\Value('string', 'string')),
+            $parser->parseConstant( $access )
+        );
+
+        $this->assertHasToken( Token::T_CLASS, $access );
+    }
+
+    public function testParseConstant_StringWithDoubleQuotes ()
+    {
+        $access = \vc\Tokens\Access::buildAccess(
+            $this->oneTokenReader()
+                ->thenAName('NAME')->thenSomeSpace()
+                ->thenAnEquals()->thenSomeSpace()
+                ->thenAString('string', '"')->thenASemicolon()
+                ->thenAClass()
+        );
+
+        $parser = new \vc\Parser\Constant( new \vc\Parser\Value );
+
+        $this->assertEquals(
+            r8(new \vc\Data\Constant('NAME'))
+                ->setValue(new \vc\Data\Value('string', 'string')),
+            $parser->parseConstant( $access )
+        );
+
+        $this->assertHasToken( Token::T_CLASS, $access );
+    }
+
+    public function testParseConstant_Null ()
+    {
+        $access = \vc\Tokens\Access::buildAccess(
+            $this->oneTokenReader()
+                ->thenAName('NAME')->thenSomeSpace()
+                ->thenAnEquals()->thenSomeSpace()
+                ->thenAName('NULL')->thenASemicolon()
+                ->thenAClass()
+        );
+
+        $parser = new \vc\Parser\Constant( new \vc\Parser\Value );
+
+        $this->assertEquals(
+            r8(new \vc\Data\Constant('NAME'))
+                ->setValue(new \vc\Data\Value('NULL', 'null')),
+            $parser->parseConstant( $access )
+        );
+
+        $this->assertHasToken( Token::T_CLASS, $access );
+    }
+
+    public function testParseConstant_Heredoc ()
+    {
+        $access = \vc\Tokens\Access::buildAccess(
+            $this->oneTokenReader()
+                ->thenAName('NAME')->thenSomeSpace()
+                ->thenAnEquals()->thenSomeSpace()
+                ->thenAHereDoc('contents')->thenASemicolon()
+                ->thenAClass()
+        );
+
+        $parser = new \vc\Parser\Constant( new \vc\Parser\Value );
+
+        $this->assertEquals(
+            r8(new \vc\Data\Constant('NAME'))
+                ->setValue(new \vc\Data\Value('contents', 'string')),
+            $parser->parseConstant( $access )
+        );
+
+        $this->assertHasToken( Token::T_CLASS, $access );
+    }
+
+}
+
+?>
