86b0b7141e8e7c7e8c941d76a5dd78d37bed7414
Nycto <James@RoundEights.com>
2010-09-10 08:06:16 -0700

    Added Storage definition

---
+++ src/ifaces/Storage.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of phpVocab.
+ *
+ * phpVocab is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * phpVocab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with phpVocab. If not, see <http://www.phpVocab.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ */
+
+namespace vc\iface;
+
+/**
+ * Stores a parsed file while reading the entire source tree
+ */
+interface Storage
+{
+
+    /**
+     * Stores the contents of the given parsed file for later analysis
+     *
+     * @param \vc\Data\File $file
+     * @return NULL
+     */
+    public function store ( \vc\Data\File $file );
+
+}
+
+?>
